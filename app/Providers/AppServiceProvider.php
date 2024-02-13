9834365f23b162fafddab5f4bc6473e4a46c2f3c
Bolaji Oluitan <bjsteelbj@gmail.com>
2024-02-13 17:14:46 +0100

    modified:   app/Providers/AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
         Blade::directive('admin_ass', function ($asset) {
             return "<?php echo 'https://jalvinhomes.com/public/backend/'.$asset; ?>";
         });
+        Schema::defaultStringLength(191);
     }
 }
