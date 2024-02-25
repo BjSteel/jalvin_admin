a2204530fb4f0868879194a2212ba465d96a1fe3
Bolaji Oluitan <bjsteelbj@gmail.com>
2024-02-25 21:22:04 +0100

    modified:   app/Http/Controllers/BackendController.php

--- app/Http/Controllers/BackendController.php
+++ app/Http/Controllers/BackendController.php
@@ -9,23 +9,23 @@ class BackendController extends Controller
     public function index(){
         $data = array();
         $data['total_properties'] = Property::count();;
-        $data['residential'] = countWordOccurrences('list','property_type', 'residential');
-        $data['commercial'] = countWordOccurrences('list','property_type', 'commercial');
-        $data['fully_detached_duplex'] = countWordOccurrences('list','property_type', 'fully_detached_duplex');
-        $data['semi_detached_duplex'] = countWordOccurrences('list','property_type', 'semi_detached_duplex');
-        $data['terrace_duplex'] = countWordOccurrences('list','property_type', 'terrace_duplex');
-        $data['bungalow'] = countWordOccurrences('list','property_type', 'bungalow');
-        $data['apartment'] = countWordOccurrences('list','property_type', 'apartment');
-        $data['maisonette'] = countWordOccurrences('list','property_type', 'maisonette');
-        $data['penthouse'] = countWordOccurrences('list','property_type', 'penthouse');
-        $data['residential_land'] = countWordOccurrences('list','property_type', 'residential_land');
-        $data['commercial_land'] = countWordOccurrences('list','property_type', 'commercial_land');
-        $data['office'] = countWordOccurrences('list','property_type', 'office');
-        $data['abuja'] = countWordOccurrences('list','property_type', 'abuja');
-        $data['lagos_island'] = countWordOccurrences('list','property_type', 'lagos_island');
-        $data['lagos_mainland'] = countWordOccurrences('list','property_type', 'lagos_mainland');
-        $data['ibadan'] = countWordOccurrences('list','property_type', 'ibadan');
-        $data['dubai'] = countWordOccurrences('list','property_type', 'dubai');
+        $data['residential'] = countWordOccurrences('list','type', 'residential');
+        $data['commercial'] = countWordOccurrences('list','type', 'commercial');
+        $data['fully_detached_duplex'] = countWordOccurrences('list','type', 'fully_detached_duplex');
+        $data['semi_detached_duplex'] = countWordOccurrences('list','type', 'semi_detached_duplex');
+        $data['terrace_duplex'] = countWordOccurrences('list','type', 'terrace_duplex');
+        $data['bungalow'] = countWordOccurrences('list','type', 'bungalow');
+        $data['apartment'] = countWordOccurrences('list','type', 'apartment');
+        $data['maisonette'] = countWordOccurrences('list','type', 'maisonette');
+        $data['penthouse'] = countWordOccurrences('list','type', 'penthouse');
+        $data['residential_land'] = countWordOccurrences('list','type', 'residential_land');
+        $data['commercial_land'] = countWordOccurrences('list','type', 'commercial_land');
+        $data['office'] = countWordOccurrences('list','type', 'office');
+        $data['abuja'] = countWordOccurrences('list','type', 'abuja');
+        $data['lagos_island'] = countWordOccurrences('list','type', 'lagos_island');
+        $data['lagos_mainland'] = countWordOccurrences('list','type', 'lagos_mainland');
+        $data['ibadan'] = countWordOccurrences('list','type', 'ibadan');
+        $data['dubai'] = countWordOccurrences('list','type', 'dubai');
 
         return view('backend.properties.dashboard', $data);
     }
